73535d3c7efc375fe38d8cef4898f14ca1914b3e
William Armillei <armilleiwilliam@gmail.com>
2018-11-05 15:59:31 +0000

    Expression languages controller
    ExpressionLanguages

---
+++ src/Controller/ExpressionLanguageController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\ExpressionLanguage\ExpressionLanguage;
+
+/**
+ * @Route("/")
+ */
+class ExpressionLanguageController extends Controller
+{
+    /**
+     * @Route("/expressionLanguages", name="Expression_languages", methods="GET|POST")
+     */
+    public function expressionLanguage(): JsonResponse
+    {
+
+        $expressionLanguage = new ExpressionLanguage();
+
+        // list of random expressions
+        $expLangOneResult = $expressionLanguage->evaluate('1 == 1'); // displays 3
+
+        $expLangTwoResult = $expressionLanguage->evaluate('18 + 12'); // displays 3
+
+        $expLangThreeResult = $expressionLanguage->evaluate('test == 1', array('test' => 1)); // displays 3
+
+        $expLangFourResult = $expressionLanguage->evaluate('test * test1', array('test' => 4, 'test1' => 7)); // displays 3
+
+        $expLangFiveResult = $expressionLanguage->evaluate('test1 == 2', array('test1' => 5)); // displays 3
+
+        return new JsonResponse(
+            array(
+                "firstResult" => $expLangOneResult,
+                "secondResult" => $expLangTwoResult,
+                "thirdResult" => $expLangThreeResult,
+                "fourthResult" => $expLangFourResult,
+                "fifthResult" => $expLangFiveResult
+            )
+        );
+    }
+}
